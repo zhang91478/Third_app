b8da0a37b4ca7b4fd37becf457b718bb7c48ae63
zhangshubo <2294858483@qq.com>
2017-09-07 16:20:07 +0800

    写了打电话和搜索的按钮

--- app/src/main/java/com/example/bsz/third_app/MainActivity.java
+++ app/src/main/java/com/example/bsz/third_app/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.bsz.third_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button)findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction("android.intent.action.DIAL");
+                startActivity(intent);
+            }
+        });
+
+        Button button2 = (Button)findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction("android.intent.action.SEARCH");
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
